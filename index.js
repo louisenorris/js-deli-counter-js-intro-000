dde9e7354f4314fb0af4d46058f0fef0a21073f8
Louise Norris <louisenorris73@yahoo.co.uk>
2019-03-30 16:11:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 function takeANumber(katzDeliLine, name) {
-  console.log("Welcome, ${name}. You are number ${katzDeliLine.length} in line.");
+   katzDeliLine.push(name);
+   return ("Welcome, ${name}. You are number ${katzDeliLine.length} in line.");
 }

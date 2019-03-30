2c9d108cc11e9513c198e2b1f1ce6db6facd6f40
Louise Norris <louisenorris73@yahoo.co.uk>
2019-03-30 16:28:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 function takeANumber(katzDeliLine, name) {
+var position = katzDeliLine.length+1
    katzDeliLine.push(name);
-   return 'Welcome, ${name}. You are number ${katzDeliLine.length} in line.';
+   return "Welcome, " + name +". You are number "+ position +" in line.";
 }
 
 function nowServing (katzDeliLine) {
   if (katzDeliLine.length > 0) return `Currently serving ${katzDeliLine.shift()}`;
   else return "There is nobody waiting to be served!";
-}
-
-function currentLine () {
-  if (katzDeliLine.length > 0) return
 }

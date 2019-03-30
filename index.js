12616e25151a26072813111d7b40c5ca962ee443
Louise Norris <louisenorris73@yahoo.co.uk>
2019-03-30 16:45:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing (katzDeliLine) {
   return "Currently serving "+ name +".";
 }
 
-function currentLine(line) {
+function currentLine(katzDeliLine) {
 if (katzDeliLine.length === 0) {
   return "The line is currently empty.";
 }
